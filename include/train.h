624c3b350ca5f2ea70230f59f415ffcecfb7de37
Lina-Mar <martipoli7@gmail.com>
2025-06-07 14:20:40 +0300

    Update train.h

--- include/train.h
+++ include/train.h
@@ -2,20 +2,33 @@
 #ifndef INCLUDE_TRAIN_H_
 #define INCLUDE_TRAIN_H_
 
+#include <vector>
+#include <random>
+
 class Train {
- private:
-  struct Car {
-    bool light; // состояние лампочки
-    Car *next;
-    Car *prev;
-  };
-  int countOp; // счетчик шагов (число переходов из вагона в вагон)
-  Car *first; // точка входа в поезд (первый вагон)
- public:
-  Train();
-~Train();
-  void addCar(bool light); // добавить вагон с начальным состоянием лампочки
-  int getLength();          // вычислить длину поезда
-  int getOpCount();         // вернуть число переходов (из вагона в вагон)
+private:
+    struct Car {
+        bool light; // состояние лампочки
+        Car* next;
+        Car* prev;
+    };
+    
+    int countOp; // счетчик шагов
+    Car* first; // первый вагон
+    Car* current; // текущий вагон (для перемещения)
+    
+public:
+    Train();
+    ~Train();
+    void addCar(bool light); // добавить вагон
+    int getLength(); // вычислить длину поезда
+    int getOpCount(); // вернуть число переходов
+    
+    // Методы для перемещения по поезду
+    void moveForward();
+    void moveBackward();
+    void toggleLight();
+    bool getLightState() const;
+    void resetPosition();
 };
 #endif  // INCLUDE_TRAIN_H_
